f819a2a9b36c541b8f56baf8f208d5faf45d26cc
jeigonin <j.igonin.63@gmail.com>
2019-04-03 22:57:48 +0200

    correction git

---
+++ CSV/src/main/scala/Program$.java
@@ -0,0 +1,13 @@
+object Program {
+  def main(args: Array[String]): Unit = {
+     var test = "e";
+  }
+
+  def whichType[A](value: Option[A])= {
+    val err = value.getOrElse("Null")
+    value match {
+
+    }
+  }
+
+}
---
+++ CSV/src/main/scala/simplePackage/Cat.scala
@@ -0,0 +1,5 @@
+package com.fakir.samples.models;
+
+case class Cat(name : String, race : String, age: Int) {
+
+}
